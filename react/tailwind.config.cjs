4fc63a02d1a2632a6937abed0298c036b92006be
Alohe <alemalohe@gmail.com>
2023-10-19 23:05:38 +0300

    Update tailwind.config.cjs

--- react/tailwind.config.cjs
+++ react/tailwind.config.cjs
@@ -2,18 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        "auto-fit": "repeat(auto-fit, minmax(300px, 1fr))",
-      },
-    },
-    colors: {
-      light: {
-        100: "#e9e9e9",
-        200: "#f5f5f5",
-        300: "#6e6e6e",
-      },
-    },
-  },
+          extend: {
+            colors: {
+              light: {
+                50: '#fafafa',
+                100: '#F0F0F0',
+                200: '#e6e6e6',
+                300: '#d9d9d9',
+                400: '#cccccc',
+                500: '#bfbfbf',
+                600: '#a6a6a6',
+                700: '#8d8d8d',
+                800: '#747474',
+              },
+              dark: {
+                100: '#BFBFBF',
+                200: '#A6A6A6',
+                300: '#7d7d7d',
+                400: '#4A4A4A',
+                500: '#212121',
+                600: '#3C3C3C',
+              },
+            },
+        },
+
   plugins: [],
 };
